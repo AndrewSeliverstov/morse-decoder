3257df9da99b9001e83c902354d99b3c3b16ae4e
AndrewSeliverstov <aardvarkd101@gmail.com>
2021-08-23 18:45:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let messageConverted = [];
+    let wordsNum = expr.split("**********");
+    
+    let newStr1 = [];
+    let newStr2 = [];
+    let newStr3 = [];
+    let newStr4 = [];
+    
+    let dot = ".";
+    let dash = "-";
+        
+    for (let i=0; i<wordsNum.length;i++){
+        let re11 = /11/gi;
+        let re10 = /10/gi;
+        let re00 = /00/gi;
+        newStr1[i] = wordsNum[i].replace(/.{1,10}(?=(.{10})+$)/g, '$& ');
+        newStr2[i] = newStr1[i].replace(re11, dash);
+        newStr3[i] = newStr2[i].replace(re10, dot);
+        newStr4[i] = newStr3[i].replace(re00, '');
+        
+    }
+    
+    let res = newStr4.join("   ");
+    
+    res.split("   ").map(function (word) {
+        word.split(" ").map(function (letter) {
+            messageConverted.push(MORSE_TABLE[letter]);
+        });
+        messageConverted.push(" ");
+    });
+
+    let res2 = messageConverted.join("");
+    let result = res2.replace(/\s+$/, '');
+    
+    return result;
 }
 
 module.exports = {
